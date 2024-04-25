4da3b542a78da142d5836cb128aab36d96ae49bc
He Qian Wang <heqian.wang@gsoft.com>
2024-04-25 16:33:21 -0400

    Temporary solution until statuscode resolve

--- src/Workleap.Extensions.OpenAPI/TypedResult/ExtractSchemaTypeResultFilter.cs
+++ src/Workleap.Extensions.OpenAPI/TypedResult/ExtractSchemaTypeResultFilter.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -6,26 +6,67 @@ namespace Workleap.Extensions.OpenAPI.TypedResult;
 
 public class ExtractSchemaTypeResultFilter : IOperationFilter
 {
+    public static Dictionary<string, string> NameStatusCodeDict { get; set; } = new Dictionary<string, string>
+        {
+            {"Ok`1", "200"},
+            {"BadRequest`1", "400"},
+            {"NotFound", "404"},
+        };
     // To obtain componenets --> scans all paths. Components is probably extracting from all responses and types.
     // Easy way --> check if annotation present, if present, skip pis defer to annotation.
     // right now, 1. Start by fetching the return type.
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        var statusCode = "200";
-        
         if (operation.Responses == null)
         {
             operation.Responses = new OpenApiResponses();
         }
         
-        if (!operation.Responses.TryGetValue(statusCode, out OpenApiResponse response))
+        // skip if annotation is present
+        if(context.MethodInfo.CustomAttributes.Any(x =>x.AttributeType == typeof(Microsoft.AspNetCore.Mvc.ProducesResponseTypeAttribute) || x.AttributeType.BaseType == typeof(Microsoft.AspNetCore.Mvc.ProducesResponseTypeAttribute)))
         {
-            response = new OpenApiResponse();
+            return;
         }
         
-        operation.Responses[statusCode] = response;
-        var schema = context.SchemaGenerator.GenerateSchema(context.MethodInfo.ReturnType.GenericTypeArguments.FirstOrDefault(), context.SchemaRepository);
-        
-        response.Content.Add("application/json", new OpenApiMediaType { Schema = schema });
+        foreach (var (statusCode, type) in this.GetResponseTypes(context))
+        {
+            if (!operation.Responses.TryGetValue(statusCode, out OpenApiResponse? response))
+            {
+                response = new OpenApiResponse();
+            }
+            
+            operation.Responses[statusCode] = response;
+            if(type != null)
+            {
+                var schema = context.SchemaGenerator.GenerateSchema(type, context.SchemaRepository);
+                
+                response.Content.Add("application/json", new OpenApiMediaType { Schema = schema });    
+            }
+            if (string.IsNullOrEmpty(response.Description))
+            {
+                response.Description = statusCode;
+            }
+        }
     }
+    
+    private IEnumerable<(string, Type)> GetResponseTypes(OperationFilterContext context)
+    {
+        var responseTypes = context.MethodInfo.ReturnType.GenericTypeArguments;
+        if (responseTypes.Length == 1 && !responseTypes.First().IsGenericType)
+        {
+            yield return (NameStatusCodeDict[context.MethodInfo.ReturnType.Name], responseTypes.First());   
+        }
+        else 
+        {
+            foreach (var responseType in responseTypes)
+            {
+                yield return (NameStatusCodeDict[responseType.Name], responseType.GenericTypeArguments.FirstOrDefault())!;
+            }
+        }
+        // if(context.MethodInfo.ReturnType.IsGenericType && context.MethodInfo.ReturnType.GetGenericTypeDefinition() == typeof(IValueHttpResult<>))
+        // {
+        //     var responseReturn = context.MethodInfo.ReturnType as IStatusCodeHttpResult;
+        //     yield return (responseReturn!.StatusCode.ToString(), context.MethodInfo.ReturnType.GenericTypeArguments.FirstOrDefault())!;
+        // }
+    } 
 }
--- src/tests/WebApi.OpenAPI.SystemTest/ExtractTypeResult/TypedResultController.cs
+++ src/tests/WebApi.OpenAPI.SystemTest/ExtractTypeResult/TypedResultController.cs
@@ -18,34 +18,34 @@ public class TypedResultController : ControllerBase
         return TypedResults.Ok(new TypedResultExample("Example"));
     }
     
-    // [HttpGet]
-    // [Route("/withNoAnnotation")]
-    // // [ProducesResponseType(typeof(TypedResultExample), StatusCodes.Status200OK)]
-    // // [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-    // // [ProducesResponseType( StatusCodes.Status404NotFound)]
-    // public Results<Ok<TypedResultExample>, BadRequest<ProblemDetails>, NotFound> TypedResultWithNoAnnotation(int id)
-    // {
-    //     return id switch
-    //     {
-    //         < 0 => TypedResults.NotFound(),
-    //         0 => TypedResults.BadRequest(new ProblemDetails()),
-    //         _ => TypedResults.Ok(new TypedResultExample("Example"))
-    //     };
-    // }
-    
-    // [HttpGet]
-    // [Route("/withSwaggerResponseAnnotation")]
-    // [SwaggerResponse(StatusCodes.Status200OK, "Returns typedresult", typeof(TypedResultExample), "application/json")]
-    // public Ok<ProblemDetails> TypedResultWithSwaggerResponseAnnotation()
-    // {
-    //     return TypedResults.Ok(new ProblemDetails());
-    // }
-    
-    // [HttpGet]
-    // [Route("/producesResponseTypeAnnotation")]
+    [HttpGet]
+    [Route("/withNoAnnotation")]
     // [ProducesResponseType(typeof(TypedResultExample), StatusCodes.Status200OK)]
-    // public Ok<ProblemDetails> TypedResultWithProducesResponseTypeAnnotation()
-    // {
-    //     return TypedResults.Ok(new ProblemDetails());
-    // }
+    // [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    // [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Results<Ok<TypedResultExample>, BadRequest<ProblemDetails>, NotFound> TypedResultWithNoAnnotation(int id)
+    {
+        return id switch
+        {
+            < 0 => TypedResults.NotFound(),
+            0 => TypedResults.BadRequest(new ProblemDetails()),
+            _ => TypedResults.Ok(new TypedResultExample("Example"))
+        };
+    }
+    
+    [HttpGet]
+    [Route("/withSwaggerResponseAnnotation")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Returns TypedResult", typeof(TypedResultExample), "application/json")]
+    public Ok<ProblemDetails> TypedResultWithSwaggerResponseAnnotation()
+    {
+        return TypedResults.Ok(new ProblemDetails());
+    }
+    
+    [HttpGet]
+    [Route("/producesResponseTypeAnnotation")]
+    [ProducesResponseType(typeof(TypedResultExample), StatusCodes.Status200OK)]
+    public Ok<ProblemDetails> TypedResultWithProducesResponseTypeAnnotation()
+    {
+        return TypedResults.Ok(new ProblemDetails());
+    }
 }
