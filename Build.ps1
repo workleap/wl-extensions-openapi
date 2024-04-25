f8a90db7d4f3a527d9c86ace2fb448f4c2b3f252
He Qian Wang <heqian.wang@gsoft.com>
2024-04-25 10:39:58 -0400

    Add system tests to build.ps1

--- Build.ps1
+++ Build.ps1
@@ -4,6 +4,8 @@ Begin {
     $ErrorActionPreference = "stop"
 }
 
+
+
 Process {
     function Exec([scriptblock]$Command) {
         & $Command
@@ -11,19 +13,47 @@ Process {
             throw ("An error occurred while executing command: {0}" -f $Command)
         }
     }
-    
+
+    function Compare-GeneratedAndExpectedFiles {
+        param(
+            [Parameter(Mandatory=$true)]
+            [string]$generatedFilePath,
+
+            [Parameter(Mandatory=$true)]
+            [string]$expectedFilePath
+        )
+
+        # Compare the generated file with the expected file
+        $generatedFileContent = Get-Content -Path $generatedFilePath
+        $expectedFileContent = Get-Content -Path $expectedFilePath
+        $diff = Compare-Object -ReferenceObject $generatedFileContent -DifferenceObject $expectedFileContent
+
+        if ($diff) {
+            $diff | Format-Table
+            Write-Error "The generated file does not match the expected file."
+            exit 1
+        } else {
+            Write-Host "The generated file matches the expected file."
+        }
+    }
+
     $workingDir = Join-Path $PSScriptRoot "src"
     $outputDir = Join-Path $PSScriptRoot ".output"
     $nupkgsPath = Join-Path $outputDir "*.nupkg"
 
+    $projectPath = Join-Path $workingDir "tests/WebApi.OpenAPI.SystemTest"
+    $generatedFilePath = Join-Path $projectPath "openapi-v1.yaml"
+    $expectedFilePath = Join-Path $workingDir "tests/expected-openapi-document.yaml"
+
+
     try {
         Push-Location $workingDir
         Remove-Item $outputDir -Force -Recurse -ErrorAction SilentlyContinue
-    
+
         Exec { & dotnet clean -c Release }
         Exec { & dotnet build -c Release }
         Exec { & dotnet test  -c Release --no-build --results-directory "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
-        Exec { & ".\tests\RunSystemTest.ps1" }
+        Exec { & Compare-GeneratedAndExpectedFiles -generatedFilePath $generatedFilePath -expectedFilePath $expectedFilePath }
         Exec { & dotnet pack  -c Release --no-build -o "$outputDir" }
 
         if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
