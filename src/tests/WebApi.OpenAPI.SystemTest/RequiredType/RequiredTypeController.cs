e0cbc658e29a7709717c3aff3a49b2a4ea54d885
He Qian Wang <heqian.wang@gsoft.com>
2024-05-08 14:10:19 -0400

    Add test covering regular nested class definition

--- src/tests/WebApi.OpenAPI.SystemTest/RequiredType/RequiredTypeController.cs
+++ src/tests/WebApi.OpenAPI.SystemTest/RequiredType/RequiredTypeController.cs
@@ -13,12 +13,46 @@ public class RequiredTypeController : ControllerBase
     {
         return this.Ok();
     }
+
+    [HttpGet("/classRequiredType", Name = "GetClassRequiredType")]
+    [ProducesResponseType(typeof(ParentRequiredExampleClass), StatusCodes.Status200OK)]
+    public IActionResult ClassRequiredType()
+    {
+        return this.Ok();
+    }
 }
 
 public sealed record ParentRequiredExample(string Id)
 {
     public sealed record RequiredExample(IReadOnlyCollection<RequiredExample.Example> Examples, string? OptionalStringProperty)
     {
-        public sealed record Example(string RequiredId);
+        public sealed record Example(IReadOnlyCollection<Example.Result> Results, string Property)
+        {
+            public sealed record Result(string Id, string? OptionalProperty);
+        }
     }
+}
+
+public sealed class ParentRequiredExampleClass(RequiredExampleClass requiredExample, string? optionalStringProperty)
+{
+    public RequiredExampleClass RequiredExample { get; } = requiredExample;
+    public string? OptionalStringProperty { get; } = optionalStringProperty;
+}
+
+public sealed class RequiredExampleClass(IReadOnlyCollection<ExampleClass> Examples, string? OptionalStringProperty)
+{
+    public IReadOnlyCollection<ExampleClass> Examples { get; } = Examples;
+    public string? OptionalStringProperty { get; } = OptionalStringProperty;
+}
+
+public sealed class ExampleClass(IReadOnlyCollection<ResultClass> Results, string Property)
+{
+    public IReadOnlyCollection<ResultClass> Results { get; } = Results;
+    public string Property { get; } = Property;
+}
+
+public sealed class ResultClass(string Id, string? OptionalProperty)
+{
+    public string Id { get; } = Id;
+    public string? OptionalProperty { get; } = OptionalProperty;
 }
