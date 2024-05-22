86ca310805568296e127a86ef74637caa15ee89e
He Qian Wang <heqian.wang@gsoft.com>
2024-05-22 16:20:48 -0400

    undo diff

--- src/Workleap.Extensions.OpenAPI.Tests/TypedResult/ExtractSchemaTypeResultFilterTests.cs
+++ src/Workleap.Extensions.OpenAPI.Tests/TypedResult/ExtractSchemaTypeResultFilterTests.cs
@@ -13,21 +13,91 @@ public class ExtractSchemaTypeResultFilterTests
     public static IEnumerable<object[]> GetResponseMetadataData()
     {
         // Synchronous results
-        yield return new object[] { typeof(Ok<TestTypedSchema>), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { new((int)HttpStatusCode.OK, typeof(TestTypedSchema)) } };
-        yield return new object[] { typeof(Accepted<TestTypedSchema>), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { new((int)HttpStatusCode.Accepted, typeof(TestTypedSchema)) } };
-        yield return new object[] { typeof(Results<Ok<TestTypedSchema>, Accepted<TestTypedSchema>, BadRequest<ProblemDetails>, NotFound>), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { new((int)HttpStatusCode.OK, typeof(TestTypedSchema)), new((int)HttpStatusCode.Accepted, typeof(TestTypedSchema)), new((int)HttpStatusCode.BadRequest, typeof(ProblemDetails)), new((int)HttpStatusCode.NotFound, null) } };
-        yield return new object[] { typeof(Ok), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { new((int)HttpStatusCode.OK, null) } };
+        yield return new object[]
+        {
+            typeof(Ok<TestTypedSchema>),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata>
+            {
+                new((int)HttpStatusCode.OK, typeof(TestTypedSchema))
+            }
+        };
+        yield return new object[]
+        {
+            typeof(Accepted<TestTypedSchema>),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata>
+            {
+                new((int)HttpStatusCode.Accepted, typeof(TestTypedSchema))
+            }
+        };
+        yield return new object[]
+        {
+            typeof(Results<Ok<TestTypedSchema>, Accepted<TestTypedSchema>, BadRequest<ProblemDetails>, NotFound>),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata>
+            {
+                new((int)HttpStatusCode.OK, typeof(TestTypedSchema)),
+                new((int)HttpStatusCode.Accepted, typeof(TestTypedSchema)),
+                new((int)HttpStatusCode.BadRequest, typeof(ProblemDetails)),
+                new((int)HttpStatusCode.NotFound, null)
+            }
+        };
+        yield return new object[]
+        {
+            typeof(Ok),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata>
+            {
+                new((int)HttpStatusCode.OK, null)
+            }
+        };
 
         // // Asynchronous results
-        yield return new object[] { typeof(Task<Ok<TestTypedSchema>>), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { new((int)HttpStatusCode.OK, typeof(TestTypedSchema)) } };
-        yield return new object[] { typeof(Task<Results<Ok<TestTypedSchema>, BadRequest<ProblemDetails>, NotFound>>), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { new((int)HttpStatusCode.OK, typeof(TestTypedSchema)), new((int)HttpStatusCode.BadRequest, typeof(ProblemDetails)), new((int)HttpStatusCode.NotFound, null) } };
-        yield return new object[] { typeof(Task<Ok>), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { new((int)HttpStatusCode.OK, null) } };
+        yield return new object[]
+        {
+            typeof(Task<Ok<TestTypedSchema>>),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata>
+            {
+                new((int)HttpStatusCode.OK, typeof(TestTypedSchema))
+            }
+        };
+        yield return new object[]
+        {
+            typeof(Task<Results<Ok<TestTypedSchema>, BadRequest<ProblemDetails>, NotFound>>),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata>
+            {
+                new((int)HttpStatusCode.OK, typeof(TestTypedSchema)),
+                new((int)HttpStatusCode.BadRequest, typeof(ProblemDetails)),
+                new((int)HttpStatusCode.NotFound, null)
+            }
+        };
+        yield return new object[]
+        {
+            typeof(Task<Ok>),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata>
+            {
+                new((int)HttpStatusCode.OK, null)
+            }
+        };
 
         // Common types we should ignore
-        yield return new object[] { typeof(IResult), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { } };
-        yield return new object[] { typeof(IActionResult), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { } };
-        yield return new object[] { typeof(ActionResult<TestTypedSchema>), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { } };
-        yield return new object[] { typeof(TestTypedSchema), new List<ExtractSchemaTypeResultFilter.ResponseMetadata> { } };
+        yield return new object[]
+        {
+            typeof(IResult),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata> {}
+        };
+        yield return new object[]
+        {
+            typeof(IActionResult),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata> {}
+        };
+        yield return new object[]
+        {
+            typeof(ActionResult<TestTypedSchema>),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata> {}
+        };
+        yield return new object[]
+        {
+            typeof(TestTypedSchema),
+            new List<ExtractSchemaTypeResultFilter.ResponseMetadata> {}
+        };
     }
 
     public static IEnumerable<object[]> GetAttributesData()
