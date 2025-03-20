588b720d56158b688040a8ae1462eb82abc17fd7
He Qian Wang <heqian.wang@gsoft.com>
2025-03-20 12:01:29 -0400

    Undo

--- src/tests/WebApi.OpenAPI.SystemTest/Program.cs
+++ src/tests/WebApi.OpenAPI.SystemTest/Program.cs
@@ -2,30 +2,22 @@ using WebApi.OpenAPI.SystemTest;
 using WebApi.OpenAPI.SystemTest.ExtractTypeResult;
 using WebApi.OpenAPI.SystemTest.OperationId;
 
-try
-{
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.AddControllers();
-    builder.Services.AddSwagger();
+builder.Services.AddControllers();
+builder.Services.AddSwagger();
 
-    var app = builder.Build();
+var app = builder.Build();
 
-    app.AddEndpointsForOperationId();
-    app.AddEndpointsForTypedResult();
+app.AddEndpointsForOperationId();
+app.AddEndpointsForTypedResult();
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-    app.MapControllers();
+app.MapControllers();
 
-    app.Run();
-}
-catch (Exception e)
-{
-    Console.Write(e.Message);
-    throw;
-}
+app.Run();
 
 // For integration testing purposes only in order to use WebApplicationFactory<TProgram>
 public partial class Program;
