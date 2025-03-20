522f4b64526ebdb800c4915bf155cb64074e0e1f
He Qian Wang <heqian.wang@gsoft.com>
2025-03-20 11:57:15 -0400

    Test issues

--- src/tests/WebApi.OpenAPI.SystemTest/Program.cs
+++ src/tests/WebApi.OpenAPI.SystemTest/Program.cs
@@ -2,22 +2,31 @@ using WebApi.OpenAPI.SystemTest;
 using WebApi.OpenAPI.SystemTest.ExtractTypeResult;
 using WebApi.OpenAPI.SystemTest.OperationId;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddSwagger();
+    builder.Services.AddControllers();
+    builder.Services.AddSwagger();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-app.AddEndpointsForOperationId();
-app.AddEndpointsForTypedResult();
+    app.AddEndpointsForOperationId();
+    app.AddEndpointsForTypedResult();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
+}
+catch (Exception e)
+{
+    Console.Write(e.Message);
+    throw;
+}
+
 
 // For integration testing purposes only in order to use WebApplicationFactory<TProgram>
 public partial class Program;
